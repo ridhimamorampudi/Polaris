42edabbaf0c6626ef142c38d142e852f44651506
Sahil Nale <64293003+sahilnale@users.noreply.github.com>
2025-04-26 21:03:42 -0700

    college resume

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -43,6 +43,7 @@ export default function NavBar() {
                 <Link href="/activities" className="text-text-secondary hover:text-text-primary">Activities</Link>
                 <Link href="/essay" className="text-text-secondary hover:text-text-primary">Essay</Link>
                 <Link href="/counselor" className="text-text-secondary hover:text-text-primary">Counselor</Link>
+                <Link href="/resume" className="text-text-secondary hover:text-text-primary">Resume</Link>
                 <button 
                   onClick={handleSignOut}
                   className="text-text-secondary hover:text-text-primary"
