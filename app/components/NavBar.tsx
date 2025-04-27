5a47fbbee4abdaee8f0925767ffe128e5c7b8fa8
Sahil Nale <64293003+sahilnale@users.noreply.github.com>
2025-04-26 20:05:16 -0700

    created ui for chatbot

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -42,6 +42,7 @@ export default function NavBar() {
                 <Link href="/major" className="text-text-secondary hover:text-text-primary">Major</Link>
                 <Link href="/activities" className="text-text-secondary hover:text-text-primary">Activities</Link>
                 <Link href="/essay" className="text-text-secondary hover:text-text-primary">Essay</Link>
+                <Link href="/counselor" className="text-text-secondary hover:text-text-primary">Counselor</Link>
                 <button 
                   onClick={handleSignOut}
                   className="text-text-secondary hover:text-text-primary"
